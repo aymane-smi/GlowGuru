2f06bc97e9283af34da1e416c408aa7488dbf458
unknown <aymanebel2@outlook.fr>
2023-01-17 10:08:22 +0100

    just finished creating product, admin modules

---
+++ app/models/Admin.php
@@ -0,0 +1,39 @@
+<?php
+    class Admin{
+        private $db;
+        public function __construct(){
+            $this->db  = new DB();
+        }
+
+        public function login($username, $password)
+        {
+            $this->db->query("SELECT * FROM admin WHERE username = :username");
+            $this->db->bind(":username", $username);
+            $row = $this->db->single();
+            if (password_verify($password, $row->password))
+                return $row;
+            else
+                return false;
+        }
+
+        public function signup($username, $password){
+            $this->db->query("INSERT INTO admin(username, password) VALUES(:username, :password)");
+            $this->db->bind(":username", $username);
+            $this->db->bind(":password", $password);
+            $this->db->execute();
+        }
+
+        public function edit($id, $username, $password = ""){
+            $this->db->query("UPDATE admin SET username = :username WHERE id = :id");
+            $this->db->bind(":id", $id);
+            $this->db->bind(":username", $username);
+            $this->db->execute();
+            if($password === ""){
+                $this->db->query("UPDATE admin SET password = :password WHERE id = :id");
+                $this->db->bind(":id", $id);
+                $this->db->bind(":password", $password);
+                $this->db->execute();
+            }
+        }
+    }
+?>
---
+++ app/models/Product.php
@@ -0,0 +1,59 @@
+<?php
+    class Product{
+        private $db;
+        public function __construct(){
+            $this->db = new DB();
+        }
+
+        public function createProduct($name, $price, $category, $image, $description){
+            $this->db->query("INSERT INTO product(name, price, category, image, description) VALUES(:name, :price, :category, :image, :description)");
+            $this->db->bind(":name", $name);
+            $this->db->bind(":price", $price);
+            $this->db->bind(":category", $category);
+            $this->db->bind(":image", $image);
+            $this->db->bind(":description", $description);
+            $this->db->execute();
+        }
+
+        public function editProduct($id, $name, $price, $category, $image = "", $description){
+            $this->db->query("UPDATE product SET name = :name, price = :price, description = :description, category = :category WHERE id = :id");
+            $this->db->bind(":name", $name);
+            $this->db->bind(":id", $id);
+            $this->db->bind(":price", $price);
+            $this->db->bind(":category", $category);
+            $this->db->bind(":image", $image);
+            $this->db->bind(":description", $description);
+            $this->db->execute();
+            if($image !== ""){
+                $this->db->query("UPDATE product SET image = :image WHERE id = :id");
+                $this->db->bind(":image", $image);
+                $this->db->bind(":id", $id);
+                $this->db->execute();
+            }
+        }
+
+        public function getAllProduct(){
+            $this->db->query("SELECT * FROM product");
+            return $this->db->resultSet();
+        }
+
+        public function deleteProductById($id){
+            $this->db->query("DELETE product WHERE id = :id");
+            $this->db->bind(":id", $id);
+            $this->db->execute();
+        }
+
+        public function getProductByCategory($category){
+            $this->db->query("SELECT * FROM product WHERE category = :category");
+            $this->db->bind(":category", $category);
+            return $this->db->resultSet();
+        }
+
+
+        public function getProductBySearch($search){
+            $this->db->query("SELECT * FROM product WHERE name LIKE %:search%");
+            $this->db->bind(":search", $search);
+            return $this->db->resultSet();
+        }
+    }
+?>
