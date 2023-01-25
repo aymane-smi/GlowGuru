b4fa5c1803b2c15e798d03332acdb6dbe8e00bbb
aymane-smi <aymanebel2@outlook.fr>
2023-01-25 16:35:18 +0100

    making login page responsive

--- app/views/Login.php
+++ app/views/Login.php
@@ -2,12 +2,12 @@
 require_once "inc/header.php";
 ?>
 <main class="w-full flex h-screen">
-    <div class="w-1/2 flex flex-col justify-start items-center h-fit mt-[5%]">
-        <p class="text-[60px] text-black logo">GlowGuru</p>
+    <div class="w-1/2 flex flex-col justify-start items-center h-fit mt-[5%] max-[780px]:w-full">
+        <a href="/" class="text-[60px] text-black logo max-[780px]:text-[40px]">GlowGuru</a>
         <form action="http://localhost:9000/Login/handleLogin" method="POST" class="mt-10 flex flex-col justify-center items-center gap-10">
-            <input id="username" type="text" name="username" class="w-[300px] p-3 border-b-[1px] border-gray-400 focus:outline-none" placeholder="username" />
-            <input id="password" type="password" name="password" class="w-[300px] p-3 border-b-[1px] border-gray-400 focus:outline-none" placeholder="password" />
-            <button class="w-[300px] p-3 bg-black text-white font-semibold rounded-md">Connecter</button>
+            <input id="username" type="text" name="username" class="max-[780px]:w-[200px] w-[300px] p-3 border-b-[1px] border-gray-400 focus:outline-none" placeholder="username" />
+            <input id="password" type="password" name="password" class="max-[780px]:w-[200px] w-[300px] p-3 border-b-[1px] border-gray-400 focus:outline-none" placeholder="password" />
+            <button class="max-[780px]:w-[200px] w-[300px] p-3 bg-black text-white font-semibold rounded-md">Connecter</button>
         </form>
         <?php
         if (isset($_SESSION["login_err"])) {
@@ -22,7 +22,7 @@ require_once "inc/header.php";
         }
         ?>
     </div>
-    <img class="w-1/2 h-screen" alt="login image" src="https://images.unsplash.com/photo-1555820585-c5ae44394b79?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=800&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NDExNzY2OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=800" />
+    <img class="w-1/2 h-screen max-[780px]:hidden" alt="login image" src="https://images.unsplash.com/photo-1555820585-c5ae44394b79?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=800&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NDExNzY2OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=800" />
 </main>
 <?php
 require_once "inc/footer.php";
